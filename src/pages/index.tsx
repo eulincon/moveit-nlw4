76143b126364f0d4a8664d7206ec180f94c9652f
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2021-02-24 11:16:22 -0300

    Aula 3 - concluded #focopraticagrupo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,28 @@
-import Head from "next/head";
-import CompletedChallenges from "../Components/CompletedChallenges";
-import Countdown from "../Components/Countdown";
-import ExperienceBar from "../Components/ExperienceBar";
-import Profile from "../Components/Profile";
-import styles from "../styles/pages/Home.module.css";
+import Head from 'next/head'
+import ChallengeBox from '../Components/ChallengeBox'
+import CompletedChallenges from '../Components/CompletedChallenges'
+import Countdown from '../Components/Countdown'
+import ExperienceBar from '../Components/ExperienceBar'
+import Profile from '../Components/Profile'
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div></div>
-      </section>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Início | move.it</title>
+			</Head>
+			<ExperienceBar />
+			<section>
+				<div>
+					<Profile />
+					<CompletedChallenges />
+					<Countdown />
+				</div>
+				<div>
+					<ChallengeBox />
+				</div>
+			</section>
+		</div>
+	)
 }
