0aaa980718d49b0bb30ed15dfc527f7af1788b98
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2021-02-25 11:33:27 -0300

    Aula 4 - complete #neverstoplearning

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,15 +1,18 @@
-import styles from "../styles/components/Profile.module.css";
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
 
 export default function Profile() {
-  return (
-    <div className={styles.profileContainer}>
-      <img src="https://github.com/zlincon.png" alt="Lincon Brito" />
-      <div>
-        <strong>Lincon Brito</strong>
-        <p>
-          <img src="icons/level.svg" alt="Level" /> Level 1
-        </p>
-      </div>
-    </div>
-  );
+	const { level } = useContext(ChallengesContext)
+	return (
+		<div className={styles.profileContainer}>
+			<img src='https://github.com/zlincon.png' alt='Lincon Brito' />
+			<div>
+				<strong>Lincon Brito</strong>
+				<p>
+					<img src='icons/level.svg' alt='Level' /> Level {level}
+				</p>
+			</div>
+		</div>
+	)
 }
